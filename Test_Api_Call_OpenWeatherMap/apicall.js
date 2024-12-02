46b5d9cb29640f85e7586584e6a6226ab38cb7dc
Luis Fernando Fabrizzio Gutierrez Sanchez <88116637+deadborregopool@users.noreply.github.com>
2024-12-02 14:02:48 -0500

    Update apicall.js

--- Test_Api_Call_OpenWeatherMap/apicall.js
+++ Test_Api_Call_OpenWeatherMap/apicall.js
@@ -1,4 +1,4 @@
-const apiKey = "9890ac87c6973b9f36a6a161b724b21d"; // Replace with your API Key
+const apiKey = "9890ac87c6973b9f36a6a161b724b21d"; 
 const city = "Lima"; // City to query
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=en`;
 
@@ -11,7 +11,6 @@ const getWeather = async () => {
     const data = await response.json();
     console.log("Weather data:", data);
 
-    // Example: Display results
     console.log(`Temperature in ${city}: ${data.main.temp}°C`);
     console.log(`Description: ${data.weather[0].description}`);
   } catch (error) {
